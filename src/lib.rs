f6df2c89b1452745d6c2e7fa3212b4641cf192c5
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2025-02-28 17:43:07 +0100

    Use clippy to clean up code

--- src/lib.rs
+++ src/lib.rs
@@ -61,7 +61,7 @@ impl Sequence {
     }
 
     fn initialize(a: usize, b: usize, c: usize, maximum: Option<ItemCandidate>) -> Self {
-        let elements: BTreeSet<usize> = vec![a, b, c].iter().cloned().collect();
+        let elements: BTreeSet<usize> = [a, b, c].iter().cloned().collect();
         Self {
             current: ItemCandidate::Index(0, vec![a, b, c], c + 1),
             elements,
