e6efed572d0fb6084daa4ac215d2c85b8e155603
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2025-03-01 15:26:18 +0100

    Create a seperate item module

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,12 @@
-use std::cmp::Ordering;
+use crate::item::ItemCandidate;
 use std::collections::BTreeSet;
 use std::ops::Bound;
 
 pub mod character;
 pub mod combinatorics;
+pub mod item;
 pub mod period;
 pub mod word;
-
-#[derive(Debug, PartialEq, Eq)]
-enum ItemCandidate {
-    Index(usize, Vec<usize>),
-    Element(usize),
-}
-
-impl ItemCandidate {
-    fn next(&self) -> Self {
-        match self {
-            ItemCandidate::Index(index, initial) if (*index + 1) < initial.len() => {
-                ItemCandidate::Index(index + 1, initial.clone())
-            }
-            ItemCandidate::Index(_, initial) => {
-                ItemCandidate::Element(initial[initial.len() - 1] + 1)
-            }
-            ItemCandidate::Element(n) => ItemCandidate::Element(*n + 1),
-        }
-    }
-}
-
-impl Ord for ItemCandidate {
-    fn cmp(&self, other: &Self) -> Ordering {
-        match self {
-            ItemCandidate::Index(i, _) => match other {
-                ItemCandidate::Index(j, _) => i.cmp(j),
-                ItemCandidate::Element(_) => Ordering::Less,
-            },
-            ItemCandidate::Element(n) => match other {
-                ItemCandidate::Index(_, _) => Ordering::Greater,
-                ItemCandidate::Element(m) => n.cmp(m),
-            },
-        }
-    }
-}
-
-impl PartialOrd for ItemCandidate {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Option::Some(self.cmp(other))
-    }
-}
 
 #[derive(Debug)]
 pub struct Sequence {
@@ -137,17 +97,6 @@ pub fn express(n: usize, elements: &BTreeSet<usize>) -> ExpressionResult {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn item_candidates_are_ordered() {
-        assert!(ItemCandidate::Index(0, vec![1, 3, 5]) < ItemCandidate::Index(1, vec![1, 3, 5]));
-        assert!(ItemCandidate::Index(2, vec![1, 3, 5]) > ItemCandidate::Index(1, vec![1, 3, 4]));
-        assert!(ItemCandidate::Index(0, vec![1, 3, 5]) < ItemCandidate::Element(51));
-        assert!(ItemCandidate::Index(1, vec![1, 3, 5]) < ItemCandidate::Element(51));
-        assert!(ItemCandidate::Index(2, vec![1, 3, 5]) < ItemCandidate::Element(51));
-        assert!(ItemCandidate::Element(37) < ItemCandidate::Element(51));
-        assert!(ItemCandidate::Element(51) > ItemCandidate::Element(37));
-    }
 
     #[test]
     fn sequence_computes_correct_elements() {
