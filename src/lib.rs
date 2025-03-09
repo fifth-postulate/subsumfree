1f520d88e606e0c9778520371fdb88b3188dd137
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2025-03-09 15:51:20 +0100

    Create a module for expression

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
+use crate::expression::{ExpressionResult, express};
 use crate::item::ItemCandidate;
 use std::collections::BTreeSet;
-use std::ops::Bound;
 
 pub mod character;
 pub mod combinatorics;
+pub mod expression;
 pub mod item;
 pub mod period;
 pub mod word;
@@ -65,32 +66,6 @@ impl Iterator for Sequence {
             self.current = self.current.next()
         }
         result
-    }
-}
-
-#[derive(Debug)]
-pub enum ExpressionResult {
-    Unexpressable,
-    Expressable(Vec<[usize; 3]>),
-}
-
-pub fn express(n: usize, elements: &BTreeSet<usize>) -> ExpressionResult {
-    let mut expressions = vec![];
-    for p in elements.range(1..n) {
-        for q in elements.range((Bound::Excluded(*p), Bound::Excluded(n))) {
-            if p + q >= n {
-                break;
-            }
-            let r = n - p - q;
-            if *q < r && elements.contains(&r) {
-                expressions.push([*p, *q, r]);
-            }
-        }
-    }
-    if expressions.is_empty() {
-        ExpressionResult::Unexpressable
-    } else {
-        ExpressionResult::Expressable(expressions)
     }
 }
 
